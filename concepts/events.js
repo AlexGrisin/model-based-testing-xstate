cfdbc0f60c230c18c80a55d91e5f772f6d448f4f
Alexandr Grisin <agrisin@griddynamics.com>
2025-02-18 14:49:06 +0200

    cleanup

--- concepts/events.js
+++
@@ -1,5 +0,0 @@
-export async function logInWithCredentials(page, user, password) {
-  await page.locator("#j_username").fill(user);
-  await page.locator("#j_password").fill(password);
-  await page.locator(".checkout-formButton--save").click();
-}
